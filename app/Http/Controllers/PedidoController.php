2c517caa8cfa9e34ae40a0fdd24ec9959bd23adf
Camila <camila.f19@aluno.ifsc.edu.br>
2023-11-24 19:23:31 -0300

    formulário pedido preenchido certo

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,88 +2,147 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProdutoStoreRequest;
-use App\Http\Requests\ProdutoUpdateRequest;
-use App\Models\Produto;
-use App\Models\TipoProduto;
-use Illuminate\Http\RedirectResponse;
+use App\Models\Pedido;
+use App\Models\Cartao;
+use App\Models\User;
+use App\Models\FormaPagamentoTipoCartao;
+use App\Models\Status;
+use App\Models\ItemVenda;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
-class ProdutoController extends Controller
+class PedidoController extends Controller
 {
-    public function index(Request $request): View
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $produtos = Produto::all();
-
-        return view('produto.list', compact('produtos'));
+        $pedidos = Pedido::all();
+        return view('pedido.list')->with(['pedidos'=> $pedidos]);
     }
 
-    public function create(Request $request): View
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        $tipo = TipoProduto::orderBy('nome')->get();
+        $usuario = User::orderBy('name')->get();
+        $pagamento = FormaPagamentoTipoCartao::orderBy('nome')->get();
+        $cartao = Cartao::orderBy('nomeTitular')->get();
+        $status = Status::orderBy('nome')->get();
 
-        return view('produto.form')->with(['tipo'=>$tipo]);
+        return view('pedido.form')->with([
+            'usuario'=> $usuario,
+            'pagamento'=> $pagamento,
+            'cartao'=> $cartao,
+            'status'=> $status ]);
     }
 
-    public function store(ProdutoStoreRequest $request): RedirectResponse
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
     {
-        $produto = Produto::create($request->validated());
+        $request->validate([
+            //'usuario_id'=>'required|max:10',
+            //'forma_pagamento_id'=>'required|max:10',
+            //'cartao_id'=>'required|max:10',
+            //'status_id'=>'required|max:10',
+            'observacao'=>'required|max:200',
+        ],[
+            //'usuario_id.required'=>"O :attribute é obrigatório!",
+            //'forma_pagamento_id.required'=>"O :attribute é obrigatório!",
+            //'cartao_id.required'=>"O :attribute é obrigatório!",
+            'observacao.max'=>" Só é permitido 200 caracteres em :attribute !",
+        ]);
 
-        $request->session()->flash('produto.id', $produto->id);
+        $dados = ['usuario_id'=> $request->usuario_id,
+            'forma_pagamento_id'=> $request->forma_pagamento_id,
+            'cartao_id'=> $request->cartao_id,
+            'status_id'=> $request->status_id,
+            'observacao'=>$request->observacao,
+        ];
 
-        return redirect()->route('produto.index');
+        Pedido::create($dados);
+
+        return redirect('pedido')->with('success', "Cadastrado com sucesso!");
     }
 
-    public function show(Request $request, Produto $produto): View
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
     {
-        return view('produto.show', compact('produto'));
+        $pedido = Pedido::find($id);
+
+        $usuario = User::orderBy('name')->get();
+        $pagamento = FormaPagamentoTipoCartao::orderBy('nome')->get();
+        $cartao = Cartao::orderBy('nomeTitular')->get();
+        $status = Status::orderBy('nome')->get();
+        //$venda = ItemVenda::orderBy('pedido_id')->get();
+
+        return view('pedido.form')->with([
+        'pedido'=>$pedido,
+        'usuario'=> $usuario,
+        'pagamento'=> $pagamento,
+        'cartao'=> $cartao,
+        'status'=> $status ]);
     }
 
-    public function edit(Request $request, Produto $produto): View
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pedido $pedido)
     {
-        $produto = Produto::find($request->id);
+        $request->validate([
+            //'usuario_id'=>'required|max:10',
+            //'forma_pagamento_id'=>'required|max:10',
+            //'cartao_id'=>'required|max:10',
+            //'status_id'=>'required|max:10',
+            'observacao'=>'required|max:200',
+        ],[
+            //'usuario_id.required'=>"O :attribute é obrigatório!",
+            //'forma_pagamento_id.required'=>"O :attribute é obrigatório!",
+            //'cartao_id.required'=>"O :attribute é obrigatório!",
+            'observacao.max'=>" Só é permitido 200 caracteres em :attribute !",
+        ]);
 
-        $tipo = TipoProduto::orderBy('nome')->get();
+        $dados = ['usuario_id'=> $request->usuario_id,
+            'forma_pagamento_id'=> $request->forma_pagamento_id,
+            'cartao_id'=> $request->cartao_id,
+            'status_id'=> $request->status_id,
+            'observacao'=>$request->observacao,
+        ];
 
-        return view('cartao.form')->with([
-        'produto'=> $produto,
-        'tipo'=> $tipo]);
+        Pedido::UpdateOrCreate(
+            ['id'=>$request->id],
+            $dados);
+
+        return redirect('pedido')->with('success', "Atualizado com sucesso!");
     }
 
-    public function update(ProdutoUpdateRequest $request, Produto $produto): RedirectResponse
-    {$request->validated();
-
-    Produto::UpdateOrCreate(
-        ['id'=>$request->id],
-        $produto);
-
-    return redirect('produto')->with('success', "Atualizado com sucesso!");
-
-    }
-
-    public function destroy(Request $request, Produto $produto): RedirectResponse
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
     {
-        $produto = Produto::findOrFail($request->id);
+        $pedido = Pedido::findOrFail($id);
 
-        $produto->delete();
+        $pedido->delete();
 
-        return redirect('produto')->with('success', "Deletado com sucesso!");
-
+        return redirect('pedido')->with('success', "Deletado com sucesso!");
     }
 
     public function search(Request $request)
     {
-        if (!empty($request->valor)) {
-            $produtos = Produto::where(
-                $request->tipo,
-                'like',
-                "%" . $request->valor . "%"
-            )->get();
-        } else {
-            $produtos = Produto::all();
+        if(!empty($request->valor)){
+            $pedido = Pedido::where($request->tipo, 'like', "%". $request->valor."%")->get();
+        }
+        else {
+            $pedido = Pedido::all();
         }
 
-        return view('produto.list')->with(['produtos' => $produtos]);
+        return view('pedido.list')->with(['pedido'=> $pedido]);
     }
+
 }
