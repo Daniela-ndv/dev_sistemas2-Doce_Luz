ddd54b4f5a4cd5d638ec1f512e77adcbd989b4bd
Camila <camila.f19@aluno.ifsc.edu.br>
2023-11-29 17:31:45 -0300

    perfil

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,39 +1,32 @@
 @section("titulo", 'profile')
 @section('content')
 
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
-        </div>
+<div class="py-12">
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow shadow-pink-600 sm:rounded-lg">
+      <div class="max-w-xl">
+        @include('profile.partials.update-profile-information-form')
+      </div>
     </div>
-</x-app-layout>
+
+    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow shadow-pink-600 sm:rounded-lg">
+      <div class="max-w-xl">
+        @include('profile.partials.update-password-form')
+      </div>
+    </div>
+
+    <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow shadow-pink-600 sm:rounded-lg">
+      <div class="max-w-xl">
+        @include('profile.partials.delete-user-form')
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
 
 <!doctype html>
 <html>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,19 +34,9 @@
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
   <link rel="shortcut icon" href="./assets/img/favicon.ico" />
-    <link
-      rel="apple-touch-icon"
-      sizes="76x76"
-      href="./assets/img/apple-icon.png"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css"
-    />
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css" />
 
   <script>
     tailwind.config = {
@@ -70,10 +53,11 @@
 </head>
 
 <body>
-    @include('base.menu')
-    <div class="md:container md:mx-auto px-8">
-        @yield('content')
-    </div>
-    @include('base.footer')
+  @include('base.menu')
+  <div class="md:container md:mx-auto px-8">
+    @yield('content')
+  </div>
+  @include('base.footer')
 </body>
+
 </html>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,10 +1,10 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h2 class="text-lg font-medium text-pink-700 dark:text-pink-100">
             {{ __('Profile Information') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        <p class="mt-1 text-sm text-pink-500 dark:text-pink-400">
             {{ __("Update your account's profile information and email address.") }}
         </p>
     </header>
@@ -30,10 +30,10 @@
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
-                    <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
+                    <p class="text-sm mt-2 text-pink-800 dark:text-pink-200">
                         {{ __('Your email address is unverified.') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                        <button form="send-verification" class="underline text-sm text-pink-600 dark:text-pink-400 hover:text-pink-900 dark:hover:text-pink-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-pink-800">
                             {{ __('Click here to re-send the verification email.') }}
                         </button>
                     </p>
@@ -48,7 +48,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>{{ __('Salvar') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
@@ -56,8 +56,8 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                    class="text-sm text-pink-600 dark:text-pink-400"
+                >{{ __('Salvo.') }}</p>
             @endif
         </div>
     </form>
